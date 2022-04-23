82c9706b9361c6ba43d73b95aa8f24c61b6256f1
Isuru <lakshanisuru170@gmail.com>
2022-04-23 17:44:45 +0530

    add changes

--- src/app/main-window/main-window.component.ts
+++ src/app/main-window/main-window.component.ts
@@ -20,10 +20,8 @@ export interface Car{
   styleUrls: ['./main-window.component.scss']
 })
 export class MainWindowComponent implements OnInit {
-  colsProduct: any[]=[];
-  colsCar: any[]=[];
+ 
   public products:Product[] = [
-  
     {id:2, code: '2', name: '2Floor',category:'2one',quantity:'2new'},
     {id:3, code: '3', name: '3Floor',category:'3one',quantity:'3new'},
     {id:1, code: '4', name: '4Floor',category:'4one',quantity:'4new'},
@@ -31,31 +29,31 @@ export class MainWindowComponent implements OnInit {
     {id:3, code: '6', name: '6Floor',category:'6one',quantity:'6new'},
   
   ];
+  public colsProduct:any[] = [
+    { field: 'id', header: 'Id' },
+    { field: 'code', header: 'Code' },
+    { field: 'name', header: 'Name' },
+    { field: 'category', header: 'Category' },
+];
+
   public cars:Car[] = [
-  
     {id:1,  seet: 'seet',price:'1000',color:'red'},
     {id:2,  seet: 'ceel',price:'5533',color:'blue'},
     {id:3,  seet: 'seeccc',price:'33355',color:'green'},
     {id:4,  seet: 'exxx',price:'72220',color:'light'},
   
   ];
-  constructor() { }
-
-  ngOnInit(): void {
-    
-    this.colsProduct = [
-      { field: 'id', header: 'Id' },
-      { field: 'code', header: 'Code' },
-      { field: 'name', header: 'Name' },
-      { field: 'category', header: 'Category' },
-  ];
-  this.colsCar = [
+  public colsCar:any[]  = [
     { field: 'id', header: 'Id' },
     { field: 'seet', header: 'Seet' },
     { field: 'price', header: 'Price' },
     { field: 'color', header: 'Color' },
 ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+ 
 
   }
 
